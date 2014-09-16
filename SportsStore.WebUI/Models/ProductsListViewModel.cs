0f46df6a4ed99cd05ebea886df8e1bc81e6c73c1
KannugoPrithvi <kannugo@gmail.com>
2014-09-16 23:36:10 +0530

    SomeContent changes and badges includsion

--- SportsStore.WebUI/Models/ProductsListViewModel.cs
+++ SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -11,5 +11,7 @@ namespace SportsStore.WebUI.Models
         public IEnumerable<Product> Products { get; set; }
         public PagingInfo PagingInfo { get; set; }
         public string CurrentCategory { get; set; }
+        //public Dictionary<string, int> Quantity { get; set; }
+        
     }
 }
