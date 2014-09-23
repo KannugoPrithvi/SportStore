f943e857247995561992e568a1fd85a73e11ed3c
KannugoPrithvi <kannugo@gmail.com>
2014-09-23 22:31:34 +0530

    added capability to handle overlfow

--- SportsStore.WebUI/Scripts/CustomScripts.js
+++ SportsStore.WebUI/Scripts/CustomScripts.js
@@ -1,7 +1,38 @@
 ﻿$(document).ready(function () {
-    $('.thumbnail').hover(function () {
-        $(this).find('.slideDownCaption').css('opacity', '1');
-    }, function () {
-        $(this).find('.slideDownCaption').css('opacity', '0');
-    });    
+    $(".clampString").dotdotdot({
+        /*	The HTML to add as ellipsis. */
+        ellipsis: '... ',
+
+        /*	How to cut off the text/html: 'word'/'letter'/'children' */
+        wrap: 'word',
+
+        /*	Wrap-option fallback to 'letter' for long words */
+        fallbackToLetter: true,
+
+        /*	jQuery-selector for the element to keep and put after the ellipsis. */
+        after: null,
+
+        /*	Whether to update the ellipsis: true/'window' */
+        watch: false,
+
+        /*	Optionally set a max-height, if null, the height will be measured. */
+        height: null,
+
+        /*	Deviation for the height-option. */
+        tolerance: 0,
+
+        /*	Callback function that is fired after the ellipsis is added,
+			receives two parameters: isTruncated(boolean), orgContent(string). */
+        callback: function (isTruncated, orgContent) { },
+
+        lastCharacter: {
+
+            /*	Remove these characters from the end of the truncated text. */
+            remove: [' ', ',', ';', '.', '!', '?'],
+
+            /*	Don't add an ellipsis if this array contains 
+				the last character of the truncated text. */
+            noEllipsis: []
+        }
+    });
 });
