632a6a2f930d3f069ca1dd18758333f5361306f9
KannugoPrithvi <kannugo@gmail.com>
2014-09-23 09:23:38 +0530

    animations for thumbnails

---
+++ SportsStore.WebUI/Scripts/CustomScripts.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('.thumbnail').hover(function () {
+        $(this).find('.slideDownCaption').css('opacity', '1');
+    }, function () {
+        $(this).find('.slideDownCaption').css('opacity', '0');
+    });    
+});
