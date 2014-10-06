3d95949ace476eeb5942af8f4ef6f7fc1422908e
KannugoPrithvi <KannugoPrithvi@users.noreply.github.com>
2014-10-07 00:06:05 +0530

    added new view model classes

---
+++ SportsStore.WebUI/Models/ProductCategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.WebUI.Models
+{
+    public class ProductCategoryViewModel
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public int ProductNos { get; set; }
+    }
+    public class ProductImageViewModel
+    {
+        public int ProductID { get; set; }
+        public string ShortDescription { get; set; }
+        public decimal AltPrice { get; set; }
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
---
+++ SportsStore.WebUI/Models/ProductWithImagesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Models
+{
+    public class ProductWithImagesViewModel
+    {
+        public int ProductID { get; set; }
+        public string Description { get; set; }
+        public decimal AltPrice { get; set; }
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+
+    }
+}
