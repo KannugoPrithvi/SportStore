f61d0d42489e7d57d320a0354091d897a5157a1a
KannugoPrithvi <KannugoPrithvi@users.noreply.github.com>
2014-10-18 17:11:43 +0530

    added ProductSpecificationviewmodel

---
+++ SportsStore.WebUI.Admin/Models/ProductSpecificationViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace SportsStore.WebUI.Models
+{
+    //Details which are shown under each table in key value pairs
+    public class ProductSubConfigurationDetails
+    {
+        public string SubHead { get; set; }
+        public string SubSpec { get; set; }
+    }
+    //Combination of one specification table
+    public class ProductSpecificationDetails
+    {
+        public int ProductSpecOrder { get; set; }
+        public string ProductSpecHeading { get; set; }
+        [XmlArray]
+        public List<ProductSubConfigurationDetails> ProductConfDetails { get; set; }
+    }   
+
+}
