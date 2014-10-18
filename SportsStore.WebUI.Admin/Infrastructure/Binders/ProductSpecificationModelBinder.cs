694582a1e1b511d3063e42d8debf4d2bd1711a5f
KannugoPrithvi <KannugoPrithvi@users.noreply.github.com>
2014-10-18 17:12:31 +0530

    Added a modelbinder

---
+++ SportsStore.WebUI.Admin/Infrastructure/Binders/ProductSpecificationModelBinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+
+namespace SportsStore.WebUI.Admin.Infrastructure.Binders
+{
+    public class ProductSpecificationModelBinder : IModelBinder
+    {
+        public bool BindModel(ModelBindingExecutionContext modelBindingExecutionContext, ModelBindingContext bindingContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
