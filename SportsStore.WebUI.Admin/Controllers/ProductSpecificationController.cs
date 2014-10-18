c843691b5a6585a4454663741d8d129ed457af2e
KannugoPrithvi <KannugoPrithvi@users.noreply.github.com>
2014-10-18 17:11:08 +0530

    Added ProductSpecificationController

---
+++ SportsStore.WebUI.Admin/Controllers/ProductSpecificationController.cs
@@ -0,0 +1,24 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Admin.Controllers
+{
+    public class ProductSpecificationController : Controller
+    {
+        IProductRepository repository;
+
+        public ProductSpecificationController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return null;
+        }
+    }
+}
