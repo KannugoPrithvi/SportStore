091fcb9b090f23883beb1e2f73a367b3081303ec
KannugoPrithvi <KannugoPrithvi@users.noreply.github.com>
2014-10-26 14:54:38 +0530

    Added dynamic tables with dynamic rows

--- SportsStore.WebUI.Admin/Controllers/ProductSpecificationController.cs
+++ SportsStore.WebUI.Admin/Controllers/ProductSpecificationController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace SportsStore.WebUI.Admin.Controllers
 {
@@ -23,41 +24,44 @@ namespace SportsStore.WebUI.Admin.Controllers
         }
 
         [HttpGet]
-        public ActionResult Edit()
+        public ActionResult Edit(int productSpecificationID = 0)
         {
-            ProductSpecificationDetails specDetails = new ProductSpecificationDetails();
-            specDetails.ProductSpecHeading = "General Specifications";
-            specDetails.ProductSpecOrder = 1;
-            specDetails.ProductConfigurationDetails = GetConfigurationList();
-            return View("ProductSpecificationList",specDetails);
+            List<ProductSpecificationDetails> prodSpecDetailsList = new List<ProductSpecificationDetails>();            
+            return View("ProductSpecificationList", prodSpecDetailsList);
+
         }
-        
+
         [HttpPost]
         public ActionResult Edit(ProductSpecificationDetails ProdSpecDetails)
         {
             var request = Request.Form;
-            ProductSpecificationDetails temp = ProdSpecDetails;
+            ProductSpecificationDetails prodSpecDetails = ProdSpecDetails;
             return View(ProdSpecDetails);
         }
 
-        [ChildActionOnly]
-        public ActionResult ProductSpecification()
+        public ActionResult NewSpecificationTable(int TableIndex = 0)
         {
-            return PartialView("Partial/ProductSpecification");
+            ProductSpecificationDetails prodSpecDetails = new ProductSpecificationDetails();
+            prodSpecDetails.ProductConfigurationDetails = GetConfigurationList();
+            ViewBag.TableIndexNo = "SpecificationList_" + TableIndex;
+            ViewBag.ButtonAddIndex = "btnAddRow_" + TableIndex;
+            ViewBag.TableIndexNoJSID = "#" + ViewBag.TableIndexNo as String;
+            ViewBag.ButtonAddIndexJSID = "#" + ViewBag.ButtonAddIndex as string;
+            return PartialView("Partial/ProductSpecification", prodSpecDetails);
         }
+
         private List<ProductSubConfigurationDetails> GetConfigurationList()
         {
             List<ProductSubConfigurationDetails> configurationList = new List<ProductSubConfigurationDetails>();
             configurationList.Add(new ProductSubConfigurationDetails { SubHead = "Name_Key_1", SubSpec = "Name_Value_1" });
             configurationList.Add(new ProductSubConfigurationDetails { SubHead = "Name_Key_2", SubSpec = "Name_Value_2" });
             configurationList.Add(new ProductSubConfigurationDetails { SubHead = "Name_Key_3", SubSpec = "Name_Value_3" });
-            configurationList.Add(new ProductSubConfigurationDetails { SubHead = "Name_Key_4", SubSpec = "Name_Value_4" });
-            configurationList.Add(new ProductSubConfigurationDetails { SubHead = "Name_Key_5", SubSpec = "Name_Value_5" });
-            configurationList.Add(new ProductSubConfigurationDetails { SubHead = "Name_Key_6", SubSpec = "Name_Value_6" });
-            configurationList.Add(new ProductSubConfigurationDetails { SubHead = "Name_Key_7", SubSpec = "Name_Value_7" });
-            configurationList.Add(new ProductSubConfigurationDetails { SubHead = "Name_Key_8", SubSpec = "Name_Value_8" });
-            configurationList.Add(new ProductSubConfigurationDetails { SubHead = "Name_Key_10", SubSpec = "Name_Value_10" });
+
             return configurationList;
+        }
+        public string ConvertToXML()
+        {
+            return null;
         }
     }
 }
--- SportsStore.WebUI.Admin/Scripts/CustomScripts.js
+++ SportsStore.WebUI.Admin/Scripts/CustomScripts.js
@@ -1,45 +1,23 @@
-﻿$(document).ready(function () {
-    console.log('triggered');
-    $(".clampString").dotdotdot({
-        /*	The HTML to add as ellipsis. */
-        ellipsis: '... ',
+﻿//The below script is to add rows dynamically to table in ProductSpecificationPage
 
-        /*	How to cut off the text/html: 'word'/'letter'/'children' */
-        wrap: 'word',
+//$(function () {
+//    $("#btnAdd").click(function (e) {
+//        var itemIndex = $("#SpecificationList tr").length;
+//        e.preventDefault();
+//        console.debug("ItemIndex = " + itemIndex);
+//        var newItem = $("<tr><td style='padding:5px'><input class='form-control' type='text' id='ProductConfigurationDetails_" + (itemIndex - 1) + "__SubHead' name='ProductConfigurationDetails[" + (itemIndex - 1) + "].SubHead'/></td><td style='padding:5px'><input class='form-control' type='text' id='ProductConfigurationDetails_" + (itemIndex - 1) + "__SubSpec' name='ProductConfigurationDetails[" + (itemIndex - 1) + "].SubSpec'></td></tr>");
+//        $("#SpecificationList").append(newItem);
+//    });
+//});
 
-        ///*	Wrap-option fallback to 'letter' for long words */
-        //fallbackToLetter: true,
 
-        ///*	jQuery-selector for the element to keep and put after the ellipsis. */
-        //after: null,
+//$(document).on("click", "#btnAdd", function (e) {
+//    var itemIndex = $("#SpecificationList tr").length;
+//    e.preventDefault();
+//    console.debug("ItemIndex = " + itemIndex);
+//    var newItem = $("<tr><td style='padding:5px'><input class='form-control' type='text' id='ProductConfigurationDetails_" + (itemIndex - 1) + "__SubHead' name='ProductConfigurationDetails[" + (itemIndex - 1) + "].SubHead'/></td><td style='padding:5px'><input class='form-control' type='text' id='ProductConfigurationDetails_" + (itemIndex - 1) + "__SubSpec' name='ProductConfigurationDetails[" + (itemIndex - 1) + "].SubSpec'></td></tr>");
+//    $("#SpecificationList").append(newItem);
+//});
 
-        ///*	Whether to update the ellipsis: true/'window' */
-        //watch: false,
 
-        ///*	Optionally set a max-height, if null, the height will be measured. */
-        //height: null,
 
-        ///*	Deviation for the height-option. */
-        //tolerance: 0,
-
-        ///*	Callback function that is fired after the ellipsis is added,
-		//	receives two parameters: isTruncated(boolean), orgContent(string). */
-        //callback: function (isTruncated, orgContent) { },
-
-        //lastCharacter: {
-
-        //    /*	Remove these characters from the end of the truncated text. */
-        //    remove: [' ', ',', ';', '.', '!', '?'],
-
-        //    /*	Don't add an ellipsis if this array contains 
-		//		the last character of the truncated text. */
-        //    noEllipsis: []
-        //}
-    });
-
-    //Please make sure the below two lines are the last lines of code of this file
-    //These lines of code are to avoid resizing of the grid divs on the final page
-    $('.thumbnail').css('height', '390px');
-    $('.thumbnail').css('width', '230px');
-    $('.clampString').css('height','50px');
-});
