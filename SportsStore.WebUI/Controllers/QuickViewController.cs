8ef6af97d3e1f3036327c4cbd2efb01f08746121
KannugoPrithvi <KannugoPrithvi@users.noreply.github.com>
2014-11-16 00:22:38 -0800

    added two new files

---
+++ SportsStore.WebUI/Controllers/QuickViewController.cs
@@ -0,0 +1,84 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.ViewModels;
+using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class QuickViewController : Controller
+    {
+        IProductRepository repository;
+        public QuickViewController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        // GET: QuickView
+        //The below view has to be made parallel and include all the features like product specification, product reviews, product ratings,related products
+        //all at once. Need to write different methods for each one of them
+        //Need to create a combined view model to send to this View,as of now only sending productspecification but would change overtime
+        public ActionResult ProductView(int ProductID = 0)
+        {
+            if(ProductID == 0)
+            {
+                TempData["Message"] = "No such product exists";
+                return View();
+            }
+            else
+            {
+                //The below model is for ProductSpecification
+                CombinedProductSpecificationViewModel combinedProductSpecificationViewModel = ReturnProductSpecification(ProductID);
+                return View("ProductView",combinedProductSpecificationViewModel);
+            }
+            
+        }
+        [ChildActionOnly]
+        public PartialViewResult ProductSpecificationPartial(int ProductID = 0)
+        {
+            if (ProductID == 0)
+            {
+                return PartialView("Partial/_ProductSpecification", null);
+            }
+            else
+            {
+                //var prodSpecViewModel = 
+                return PartialView("Partial/_ProductSpecification");
+            }
+        }
+        public CombinedProductSpecificationViewModel ReturnProductSpecification(int ProductID = 0)
+        {
+            //The below code has to be moved to a different function or a method
+            var result = from ps in repository.ProductSpecifications
+                         join psa in repository.ProductSpecificationAttributes
+                         on ps.ProductSpecificationID equals psa.ProductSpecificationID
+                         where ps.ProductID == ProductID
+                         select new
+                         {
+                             ps.ProductID,
+                             ps.ProductSpecificationOrder,
+                             ps.ProductSpecificationHeader,
+                             psa.AttributeKey,
+                             psa.AttributeValue
+                         };
+            var distinctHeader = result.
+                Select(p => p.ProductSpecificationHeader).Distinct();
+            List<ProductSpecificationDetails> productSpecDetails = new List<ProductSpecificationDetails>();
+            foreach (var item in distinctHeader)
+            {
+                ProductSpecificationDetails productSpecDetailObject = new ProductSpecificationDetails();
+                productSpecDetailObject.ProductSpecHeading = item;
+                productSpecDetailObject.ProductConfigurationDetails = (from attr in result where attr.ProductSpecificationHeader.Equals(item) select new ProductSubConfigurationDetails { SubHead = attr.AttributeKey, SubSpec = attr.AttributeValue }).ToList<ProductSubConfigurationDetails>();
+                productSpecDetails.Add(productSpecDetailObject);
+            }
+            ProductSpecificationViewModel productSpecificationViewModel = new ProductSpecificationViewModel();
+            productSpecificationViewModel.ProductID = ProductID;
+            productSpecificationViewModel.lstProductSpecificationDetails = productSpecDetails;
+            CombinedProductSpecificationViewModel combinedProductSpecificationViewModel = new CombinedProductSpecificationViewModel();
+            combinedProductSpecificationViewModel.ProductSpecificationViewModel = productSpecificationViewModel;
+            return combinedProductSpecificationViewModel;
+        }
+    }
+}
---
+++ SportsStore.WebUI/Models/CombinedProductSpecificationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SportsStore.Domain.ViewModels;
+
+namespace SportsStore.WebUI.Models
+{
+    public class CombinedProductSpecificationViewModel
+    {
+        public ProductSpecificationViewModel ProductSpecificationViewModel { get; set; }
+    }
+}
