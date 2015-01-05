41a847c1334d93e2207c3b29006d8d8b5455477d
KannugoMVC <kannugo@gmail.com>
2015-01-06 00:16:29 +0530

    started implementing cart

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -80,24 +80,30 @@ namespace SportsStore.WebUI.Controllers
         {
             return View(new ShippingDetails());
         }
-        [HttpPost]
-        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
-        {
-            if (cart.Lines.Count() == 0)
-            {
-                ModelState.AddModelError("", "Sorry your cart is empty!");
-            }
+        //[HttpPost]
+        //public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        //{
+        //    if (cart.Lines.Count() == 0)
+        //    {
+        //        ModelState.AddModelError("", "Sorry your cart is empty!");
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                orderProcessor.ProcessOrder(cart, shippingDetails);
-                cart.Clear();
-                return View("Completed");
-            }
-            else
-            {
-                return View(shippingDetails);
-            }
+        //    if (ModelState.IsValid)
+        //    {
+        //        orderProcessor.ProcessOrder(cart, shippingDetails);
+        //        cart.Clear();
+        //        return View("Completed");
+        //    }
+        //    else
+        //    {
+        //        return View(shippingDetails);
+        //    }
+        //}
+        [HttpPost]
+        public ActionResult Checkout(Cart cart,CheckOutViewModel checkOutViewModel)
+        {
+            var request = Request.Form;
+            return null;
         }
     }
 }
---
+++ SportsStore.WebUI/Models/CheckOutViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.WebUI.Models
+{
+    public class CheckOutViewModel
+    {
+        public CustomerDetails BillingAddressDetails { get; set; }
+        public CustomerDetails ShippingAddressDetails { get; set; }
+        public bool IsShippingAddressChecked { get; set; }
+        public String OrderNotes { get; set; }
+        public bool IsCreateAccountChecked { get; set; }
+        public String Password { get; set; }
+        public String PaymentMode { get; set; }
+    }
+    public class CustomerDetails
+    {
+        public String Country { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Address { get; set; }
+        public String Email { get; set; }
+        public String ZipCode { get; set; }
+        public String TownCity { get; set; }
+        public String StateProvince { get; set; }
+        public String Company { get; set; }
+        public String Phone { get; set; }
+    }
+}
